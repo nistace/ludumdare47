773bf4c742e90df23bfb2878ac60328dc4a30c64
Nathan <nathan.istace@gmail.com>
2020-10-02 23:39:02 +0200

    Ready for the jam !

---
+++ Assets/Scripts/Game/Scenes/Menu/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using LD47.Ui;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LD47 {
+	public class MenuController : MonoBehaviour {
+		[SerializeField] protected MenuUi _menuUi;
+
+		private void OnEnable() => _menuUi.onStartClicked.AddListenerOnce(StartGame);
+		private void OnDisable() => _menuUi.onStartClicked.RemoveListener(StartGame);
+
+		private static void StartGame() => SceneManager.LoadScene("Game");
+	}
+}
---
+++ Assets/Scripts/Game/Scenes/Menu/Ui/MenuUi.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace LD47.Ui {
+	public class MenuUi : MonoBehaviour {
+		[SerializeField] protected Button _startButton;
+
+		public UnityEvent onStartClicked => _startButton.onClick;
+	}
+}
